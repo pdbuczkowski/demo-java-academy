deafcc8f2e8f1fa01bf7b6d4db6efe367daceaac
Piotr <p.d.buczkowski@gmail.com>
2024-04-08 10:10:00 +0200

    init

---
+++ src/main/java/com/mobica/demojavaacademy/DemoJavaAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.mobica.demojavaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobica/demojavaacademy/DemoJavaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobica.demojavaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
