bcc1a61a7473747a85bf3e6c765a9c2d87cdd1f2
Piotr <p.d.buczkowski@gmail.com>
2024-04-08 13:41:17 +0200

    devtools at work: updating webpage doesn't require to manually restart app;

--- src/main/java/com/mobica/demojavaacademy/DemoJavaAcademyApplication.java
+++ src/main/java/com/mobica/demojavaacademy/DemoJavaAcademyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoJavaAcademyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaAcademyApplication.class, args);
 	}
-
 }
