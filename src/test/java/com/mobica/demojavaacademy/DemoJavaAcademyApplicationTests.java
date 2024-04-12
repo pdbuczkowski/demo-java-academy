dd88f5a3ffc95790128c4812572af55c91e09ff3
Piotr <p.d.buczkowski@gmail.com>
2024-04-12 09:53:58 +0200

    WIP: trying to run unit tests with JUnit5;

--- src/test/java/com/mobica/demojavaacademy/DemoJavaAcademyApplicationTests.java
+++ src/test/java/com/mobica/demojavaacademy/DemoJavaAcademyApplicationTests.java
@@ -3,7 +3,7 @@ package com.mobica.demojavaacademy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoJavaAcademyApplication.class)
 class DemoJavaAcademyApplicationTests {
 
 	@Test
