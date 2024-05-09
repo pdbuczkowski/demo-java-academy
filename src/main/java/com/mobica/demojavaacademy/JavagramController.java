e5e17acf4ec8d005366dfa0ed06cd816749a577a
Piotr <p.d.buczkowski@gmail.com>
2024-05-09 18:29:20 +0200

    adding "/submitItem" handler;

--- src/main/java/com/mobica/demojavaacademy/JavagramController.java
+++ src/main/java/com/mobica/demojavaacademy/JavagramController.java
@@ -3,6 +3,7 @@ package com.mobica.demojavaacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -10,11 +11,17 @@ public class JavagramController {
 
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("user", new User());
         return "sign-up";
     }
 
     @GetMapping("/result")
     public String getResult() {
         return "result";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(User user) {
+        return "redirect:/result";
     }
 }
---
+++ src/main/java/com/mobica/demojavaacademy/User.java
@@ -0,0 +1,83 @@
+package com.mobica.demojavaacademy;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+public class User {
+
+    @NotBlank(message = "First name cannot be blank.")
+    @Size(min = 2, message = "First name is too short.")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank.")
+    @Size(min = 2, message = "Last name cannot be blank")
+    private String lastName;
+
+    @NotBlank(message = "Username cannot be blank.")
+    @Size(min = 7, message = "Username is too short.")
+    private String userName;
+
+    @Email
+    private String email;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past(message = "Date of birth must be in the past.")
+    private Date dateOfBirth;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
